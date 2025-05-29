a4b3a9650228776ea4db3d8559d3cb2bc42213d2
UltraQbik <no1skill@yandex.ru>
2025-05-29 14:51:30 +0300

    Add class SingleCPUSimulation(Board)

--- source/multicore_process.py
+++ source/multicore_process.py
@@ -1,3 +1,18 @@
 """
 Use CPU to process the board
 """
+
+
+import numpy as np
+from source.board import Board
+
+
+class SingleCPUSimulation(Board):
+    """
+    Single core CPU processed particle simulation
+    """
+
+    def simulation_step(self):
+        """
+        Makes a step in simulation
+        """
