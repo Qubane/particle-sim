cdbda2ec5a9a94762440f15f6f7db4a34b8494d8
UltraQbik <no1skill@yandex.ru>
2025-05-29 18:06:25 +0300

    Fix particles disappearing Fixed issue where some particles were disappearing

--- source/cpu_process.py
+++ source/cpu_process.py
@@ -27,60 +27,63 @@ class SingleCPUSimulation(Board):
                 # get particle (no boundary checks)
                 particle = self.board[y * self.width + x]
 
-                # if void -> skip
+                # if void / empty -> skip
                 if particle < 1:
                     continue
 
-                # neighbor particles (on left and right)
+                # any particle
+                # if particle can go down -> go
+                if self.get(x, y - 1) == 0:
+                    new[(y - 1) * self.width + x] = new[y * self.width + x]
+                    new[y * self.width + x] = 0
+                    continue
+
+                # check left and right
                 left, right = self.get(x - 1, y), self.get(x + 1, y)
 
-                # all particles
-                # if particle can go down -> do so
-                if self.get(x, y - 1) == 0:
-                    # copy particle down
-                    new[(y - 1) * self.width + x] = new[y * self.width + x]
-
-                    # delete from current position
-                    new[y * self.width + x] = 0
-
-                # both neighbors are empty
-                elif left == 0 and right == 0:
-                    # neighbors below
+                # if left and right are empty
+                if left == 0 and right == 0:
+                    # check left and right below
                     b_left, b_right = self.get(x - 1, y - 1), self.get(x + 1, y - 1)
+                    b_left2, b_right2 = self.get(x - 2, y - 1), self.get(x + 2, y - 1)
 
-                    # if both neighbors are clear
-                    if b_left == 0 and b_right == 0:
-                        # coin flip
-                        coin = random.randint(0, 1)
-
-                        if coin == 0:
+                    # if both are empty -> go
+                    if b_left == 0 and b_right == 0 and b_left2 == 0 and b_right2 == 2:
+                        # move randomly down and left or right
+                        if random.randint(0, 1):
                             new[(y - 1) * self.width + (x - 1)] = new[y * self.width + x]
                             new[y * self.width + x] = 0
                         else:
                             new[(y - 1) * self.width + (x + 1)] = new[y * self.width + x]
                             new[y * self.width + x] = 0
-                    elif b_left == 0:
+
+                    # if left is empty -> go
+                    elif b_left == 0 and b_left2 == 0:
                         new[(y - 1) * self.width + (x - 1)] = new[y * self.width + x]
                         new[y * self.width + x] = 0
-                    elif b_right == 0:
+
+                    # if right is empty -> go
+                    elif b_right == 0 and b_right2 == 0:
                         new[(y - 1) * self.width + (x + 1)] = new[y * self.width + x]
                         new[y * self.width + x] = 0
+
+                    # else -> stay
                     else:
                         pass
 
-                # only left neighbor is free
+                # if only left is empty
                 elif left == 0:
                     if self.get(x - 1, y - 1) == 0:
                         new[(y - 1) * self.width + (x - 1)] = new[y * self.width + x]
                         new[y * self.width + x] = 0
 
-                # only right neighbor is free
+                # if only right is empty
                 elif right == 0:
                     if self.get(x + 1, y - 1) == 0:
                         new[(y - 1) * self.width + (x + 1)] = new[y * self.width + x]
                         new[y * self.width + x] = 0
 
-                # no neighbors are free
+                # else -> stay
                 else:
                     pass
 
