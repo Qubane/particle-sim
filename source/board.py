027e39dbc5dc5565302cc50f30ceebe504b21d78
UltraQbik <no1skill@yandex.ru>
2025-05-29 13:26:50 +0300

    Add class Board & define its methods

--- source/board.py
+++ source/board.py
@@ -1,3 +1,43 @@
 """
 Particle board class
 """
+
+
+import numpy as np
+
+
+class Board:
+    """
+    2D grid of particles
+    """
+
+    def __init__(self, width: int, height: int):
+        self.width: int = width
+        self.height: int = height
+
+        self.board: np.ndarray = np.zeros((self.width, self.height), dtype=np.uint8)
+
+    def plot(self, x_pos: int, y_pos: int, value: int):
+        """
+        Plots a particle at a given coordinate
+        :param x_pos: x position
+        :param y_pos: y position
+        :param value: particle value (id)
+        """
+
+    def get(self, x_pos: int, y_pos: int) -> int:
+        """
+        Gets a particle from a given coordinate
+        :param x_pos: x position
+        :param y_pos: y position
+        :return: particle value (id)
+        """
+
+    def brush(self, x_pos: int, y_pos: int, value: int, size: float = 1.0):
+        """
+        Draws with a circular brush at a given coordinate
+        :param x_pos: x position
+        :param y_pos: y position
+        :param value: particle value (id)
+        :param size: brush size
+        """
