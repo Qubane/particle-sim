c1869e370e9f099489eefa4681e8b3ae4bd00c9f
UltraQbik <no1skill@yandex.ru>
2025-05-29 16:11:45 +0300

    Update application.py

--- source/application.py
+++ source/application.py
@@ -5,6 +5,7 @@ Application class
 
 import random
 import moderngl as gl
+from source.board import Board
 from source.gpu_process import *
 from source.cpu_process import SingleCPUSimulation, MultipleCPUSimulation
 
@@ -20,6 +21,23 @@ class App:
         # 1 - Multicore CPU
         # 2 - Compute GPU
         self.mode: int = 0
+
+        # width and height
+        self.width: int = 120
+        self.height: int = 60
+
+        # pick board
+        if self.mode == 0:
+            self.board: SingleCPUSimulation = SingleCPUSimulation(self.width, self.height)
+
+        elif self.mode == 1:
+            self.board: MultipleCPUSimulation = MultipleCPUSimulation(self.width, self.height)
+
+        elif self.mode == 2:
+            ...
+
+        else:
+            raise NotImplementedError
 
     def run(self):
         """
