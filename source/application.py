6b159a670fccc81bc347d77fa870558ed167ad56
UltraQbik <no1skill@yandex.ru>
2025-05-29 04:51:04 +0300

    Add self.compute_shader

--- source/application.py
+++ source/application.py
@@ -3,13 +3,21 @@ Application class
 """
 
 
+import moderngl as gl
+
+
 class App:
     """
     Application class
     """
 
     def __init__(self):
-        ...
+        self.ctx: gl.Context = gl.create_context()
+
+        with open("shaders/compute.glsl", "r", encoding="ascii") as file:
+            self.compute_shader: gl.ComputeShader = self.ctx.compute_shader(file.read())
 
     def run(self):
-        ...
+        """
+        Runs the application
+        """
