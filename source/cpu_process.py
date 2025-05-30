cb807331860d5bd368c00e92a2bcf46c9f18fdca
UltraQbik <no1skill@yandex.ru>
2025-05-31 00:46:46 +0300

    Refactor code

--- source/cpu_process.py
+++ source/cpu_process.py
@@ -24,11 +24,8 @@ class SingleCPUSimulation(Board):
         # go through all particles, and process them
         for y in range(self.height):
             for x in range(self.width):
-                # get particle (no boundary checks)
-                particle = self.board[y * self.width + x]
-
-                # if void / empty -> skip
-                if particle < 1:
+                # if self is empty -> skip
+                if self.board[y * self.width + x] < 1:
                     continue
 
                 # any particle
@@ -38,18 +35,25 @@ class SingleCPUSimulation(Board):
                     new[y * self.width + x] = 0
                     continue
 
-                # check left and right
-                left, right = self.get(x - 1, y), self.get(x + 1, y)
-                left2, right2 = self.get(x - 2, y), self.get(x + 2, y)
+                # fetch 4 particles (2 on left, 2 on right)
+                left = (
+                    self.get(x - 1, y) == 0,
+                    self.get(x - 2, y) == 0)
+                right = (
+                    self.get(x + 1, y) == 0,
+                    self.get(x + 2, y) == 0
+                )
 
-                # if left and right are empty
-                if left == 0 and left2 == 0 and right == 0 and right2 == 2:
-                    # check left and right below
-                    b_left, b_right = self.get(x - 1, y - 1), self.get(x + 1, y - 1)
+                # if all are empty
+                if all(left) and all(right):
+                    # check spaces below
+                    spaces = (
+                        self.get(x - 1, y - 1) == 0,
+                        self.get(x + 1, y - 1) == 0)
 
-                    # if both are empty -> go
-                    if b_left == 0 and b_right == 0:
-                        # move randomly down and left or right
+                    # if both space
+                    if all(spaces):
+                        # pick random space
                         if random.randint(0, 1):
                             new[(y - 1) * self.width + (x - 1)] = new[y * self.width + x]
                             new[y * self.width + x] = 0
@@ -58,34 +62,26 @@ class SingleCPUSimulation(Board):
                             new[y * self.width + x] = 0
 
                     # if left is empty -> go
-                    elif b_left == 0:
+                    elif spaces[0]:
                         new[(y - 1) * self.width + (x - 1)] = new[y * self.width + x]
                         new[y * self.width + x] = 0
 
                     # if right is empty -> go
-                    elif b_right == 0:
+                    elif spaces[1]:
                         new[(y - 1) * self.width + (x + 1)] = new[y * self.width + x]
                         new[y * self.width + x] = 0
 
-                    # else -> stay
-                    else:
-                        pass
-
                 # if only left is empty
-                elif left == 0 and left2 == 0:
+                elif all(left):
                     if self.get(x - 1, y - 1) == 0:
                         new[(y - 1) * self.width + (x - 1)] = new[y * self.width + x]
                         new[y * self.width + x] = 0
 
                 # if only right is empty
-                elif right == 0 and right2 == 0:
+                elif all(right):
                     if self.get(x + 1, y - 1) == 0:
                         new[(y - 1) * self.width + (x + 1)] = new[y * self.width + x]
                         new[y * self.width + x] = 0
-
-                # else -> stay
-                else:
-                    pass
 
         # copy the new board
         self.board = new
