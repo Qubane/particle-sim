3d962f4a8a081c412091cf277de5c89a3b781b4b
UltraQbik <no1skill@yandex.ru>
2025-05-31 03:28:10 +0300

    Add moderngl context

--- source/gpu_process.py
+++ source/gpu_process.py
@@ -4,6 +4,7 @@ Use GPU compute shaders to process the board
 
 
 import numpy as np
+import moderngl as mgl
 from source.board import Board
 
 
@@ -11,6 +12,16 @@ class GPUSimulation(Board):
     """
     GPU processed particle simulation
     """
+
+    def __init__(self, *args, **kwargs):
+        super().__init__(*args, **kwargs)
+
+        # create context
+        self.ctx = mgl.create_context(standalone=True)
+
+        # load shader
+        with open("shaders/compute.glsl", "r", encoding="utf-8") as file:
+            self._compute: mgl.ComputeShader = self.ctx.compute_shader(file.read())
 
     def simulation_step(self):
         """
