e734662af44e372084347d6a81b5f2f6f5fcf164
UltraQbik <no1skill@yandex.ru>
2025-05-31 03:16:44 +0300

    Add class GPUSimulation

--- source/gpu_process.py
+++ source/gpu_process.py
@@ -1,3 +1,18 @@
 """
 Use GPU compute shaders to process the board
 """
+
+
+import numpy as np
+from source.board import Board
+
+
+class GPUSimulation(Board):
+    """
+    GPU processed particle simulation
+    """
+
+    def simulation_step(self):
+        """
+        Makes a step in simulation
+        """
