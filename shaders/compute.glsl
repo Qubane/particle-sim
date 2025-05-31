8bf599613e43a06f478df9e003399236fec7616a
UltraQbik <no1skill@yandex.ru>
2025-05-31 05:15:04 +0300

    Add get_unbound; get_bound; put_unbound

--- shaders/compute.glsl
+++ shaders/compute.glsl
@@ -11,6 +11,20 @@ layout(std430, binding = 1) buffer ProcessedGrid {
     uint processedGrid[];
 };
 
+uint get_unbound(uvec2 pos) {
+    return particleGrid[pos.y * u_Width + pos.x];
+}
+
+uint get_bound(uvec2 pos) {
+    if (pos.x > 0 && pos.x < u_Width && pos.y > 0 && pos.y < u_Height)
+        return get_unbound(pos);
+    return 0u;
+}
+
+void put_unbound(uvec2 pos, uint val) {
+    particleGrid[pos.y * u_Width + pos.x] = val;
+}
+
 layout(local_size_x = 16, local_size_y = 16) in;
 void main() {
     // global id
