afb78e54131327783fbd7a5b0f3c06863c6b2ee9
UltraQbik <no1skill@yandex.ru>
2025-05-31 05:32:35 +0300

    Make working particle sim

--- shaders/compute.glsl
+++ shaders/compute.glsl
@@ -11,32 +11,65 @@ layout(std430, binding = 1) buffer ProcessedGrid {
     uint processedGrid[];
 };
 
-uint get_unbound(uvec2 pos) {
-    return particleGrid[pos.y * u_Width + pos.x];
+uint get_unbound(uint x, uint y) {
+    return particleGrid[y * u_Width + x];
 }
 
-uint get_bound(uvec2 pos) {
-    if (pos.x > 0 && pos.x < u_Width && pos.y > 0 && pos.y < u_Height)
-        return get_unbound(pos);
-    return 0u;
+uint get_bound(uint x, uint y) {
+    if (x > 0 && x < u_Width && y > 0 && y < u_Height)
+        return particleGrid[y * u_Width + x];
+    return 1u;
 }
 
-void put_unbound(uvec2 pos, uint val) {
-    particleGrid[pos.y * u_Width + pos.x] = val;
+void put_unbound(uint x, uint y, uint val) {
+    processedGrid[y * u_Width + x] = val;
 }
 
 layout(local_size_x = 16, local_size_y = 16) in;
 void main() {
     // global id
-    uvec2 globalId = gl_GlobalInvocationID.xy;
+    uvec2 pos = gl_GlobalInvocationID.xy;
 
     // if it exceeds bounds
-    if (globalId.x >= u_Width || globalId.y >= u_Height)
+    if (pos.x >= u_Width || pos.y >= u_Height)
         return;
 
-    // calculate index
-    uint index = globalId.y * u_Width + globalId.x;
+    // get current particle
+    uint particle = particleGrid[pos.y * u_Width + pos.x];
 
-    // do nothing basically
-    processedGrid[index] = particleGrid[index];
+    // if it's empty -> return
+    if (particle == 0)
+        return;
+
+    // any particle
+    // if particle can go down -> do & exit
+    if (get_bound(pos.x, pos.y - 1) == 0) {
+        put_unbound(pos.x, pos.y - 1, particle);
+        return;
+    }
+
+    // fetch 4 neighboring particles
+    uint left_1 = get_bound(pos.x - 1, pos.y);
+    uint left_2 = get_bound(pos.x - 2, pos.y);
+    uint right_1 = get_bound(pos.x + 1, pos.y);
+    uint right_2 = get_bound(pos.x + 2, pos.y);
+
+    // if left is empty
+    if (left_1 == 0 && left_2 == 0) {
+        // put down-left if the space is free
+        if (get_bound(pos.x - 1, pos.y - 1) == 0)
+            put_unbound(pos.x - 1, pos.y - 1, particle);
+    }
+
+    // if right is empty
+    else if (right_1 == 0 && right_2 == 0) {
+        // put down-left if the space is free
+        if (get_bound(pos.x + 1, pos.y - 1) == 0)
+            put_unbound(pos.x + 1, pos.y - 1, particle);
+    }
+
+    // otherwise just leave
+    else {
+        put_unbound(pos.x, pos.y, particle);
+    }
 }
