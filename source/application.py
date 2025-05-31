bee9e138baca9c1ba77eab5720068ceb7c62a753
UltraQbik <no1skill@yandex.ru>
2025-05-31 03:13:57 +0300

    Make proper particle rendering

--- source/application.py
+++ source/application.py
@@ -2,11 +2,11 @@
 Application class
 """
 
-
 import os
 import math
 import time
 import pygame
+from source.board import ParticleColor
 from source.gpu_process import *
 from source.cpu_process import SingleCPUSimulation
 
@@ -21,15 +21,16 @@ class App:
         pygame.init()
 
         # pygame window
-        self.window_width: int = 800
-        self.window_height: int = 600
+        self.window_width: int = 1280
+        self.window_height: int = 720
         self.window = pygame.display.set_mode((self.window_width, self.window_height))
 
         # pygame window caption
         pygame.display.set_caption("Particles")
 
         # application loop
-        self.running = False
+        self.running: bool = False
+        self.framerate: int = 25
 
         # board_mode of execution
         # 0 - Single core CPU
@@ -37,8 +38,8 @@ class App:
         self.board_mode: int = 0
 
         # grid_width and grid_height
-        self.grid_width: int = 120
-        self.grid_height: int = 30
+        self.grid_width: int = 160
+        self.grid_height: int = 90
 
         # pick board
         if self.board_mode == 0:
@@ -59,6 +60,7 @@ class App:
         self.running = True
 
         # application loop
+        clk = pygame.time.Clock()
         while self.running:
             # render image
             self.process_render()
@@ -68,6 +70,9 @@ class App:
 
             # process logic
             self.process_logic()
+
+            # wait till next frame
+            clk.tick(self.framerate)
 
     def process_events(self):
         """
@@ -83,6 +88,29 @@ class App:
         Processes window rendering
         """
 
+        # clear display
+        self.window.fill(0)
+
+        # calculate size of particles
+        particle_size_x = math.ceil(self.window_width / self.grid_width)
+        particle_size_y = math.ceil(self.window_height / self.grid_height)
+
+        # render particle board to window
+        for y in range(self.grid_height):
+            for x in range(self.grid_width):
+                # if particle is empty -> skip
+                if self.board.board[(self.grid_height - y - 1) * self.grid_width + x] < 1:
+                    continue
+
+                # calculate rectangle
+                rect = (
+                    x * particle_size_x, y * particle_size_y,  # position
+                    particle_size_x,     particle_size_y       # size
+                )
+
+                # draw rectangle
+                pygame.draw.rect(self.window, ParticleColor.SAND, rect)
+
         # update image
         pygame.display.flip()
 
@@ -93,7 +121,7 @@ class App:
 
         # sinusoidal movement for brush
         x = (math.sin(time.perf_counter() / 2) + 1) / 2 * self.grid_width
-        self.board.brush(x, self.grid_height * 0.8, 1, 3.5)
+        self.board.brush(x, self.grid_height * 0.8, 1, 3.0)
 
         # make a simulation step
         self.board.simulation_step()
