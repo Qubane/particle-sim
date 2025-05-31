6e990c85a0c9ee108b1b84b12da4e19feaa66981
UltraQbik <no1skill@yandex.ru>
2025-05-31 05:10:12 +0300

    Make a compute shader that does nothing

--- shaders/compute.glsl
+++ shaders/compute.glsl
@@ -1,14 +1,28 @@
 #version 430 core
 
+uniform uint u_Width;
+uniform uint u_Height;
+
 layout(std430, binding = 0) buffer ParticleGrid {
-    int particleGrid[];
+    uint particleGrid[];
 };
 
 layout(std430, binding = 1) buffer ProcessedGrid {
-    int processedGrid[];
+    uint processedGrid[];
 };
 
 layout(local_size_x = 16, local_size_y = 16) in;
 void main() {
-    ivec2 coord = ivec2(gl_GlobalInvocationID.xy);
+    // global id
+    uvec2 globalId = gl_GlobalInvocationID.xy;
+
+    // if it exceeds bounds
+    if (globalId.x >= u_Width || globalId.y >= u_Height)
+        return;
+
+    // calculate index
+    uint index = globalId.y * u_Width + globalId.x;
+
+    // do nothing basically
+    processedGrid[index] = particleGrid[index];
 }
